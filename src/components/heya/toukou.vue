ddd3583d0e77ff8bd2f428bd19f517ddef400fc8
LawrenceTurnerW <watawata.takatan@gmail.com>
2019-10-13 23:11:44 +0900

    [ADD]-検索フォームのコンポーネントを追加、改善の余地あり

--- src/components/heya/toukou.vue
+++ src/components/heya/toukou.vue
@@ -6,7 +6,13 @@
       <v-list-item>
         <v-list-item-avatar color="grey"></v-list-item-avatar>
         <v-list-item-content>
-          <v-list-item-title class="headline">IAchan KawaIA</v-list-item-title>
+          <v-list-item-title class="headline">
+          <div v-for="item in list"
+            v-bind:key="item.id"
+            v-bind:class="{ tuyoi: item.hp > 300 }">
+            ID.{{ item.id }} {{ item.name }} HP.{{ item.hp }}
+          </div>
+          </v-list-item-title>
           <v-list-item-subtitle>by W_Lawrence</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
@@ -17,7 +23,7 @@
       ></v-img>
 
       <v-card-text>
-        IA chanha sekaino shinri nanode minnamo shinkou siyoune.
+        IA chanha sekaino shinri nanode minnamo shinkou shiyoune.
       </v-card-text>
 
       <v-card-actions>
@@ -43,3 +49,19 @@
       </v-card-actions>
     </v-card>
 </template>
+
+<script>
+export default {
+  components: {
+  },
+  data() {
+    return {
+      list: [
+      { id: 1, name: 'スライム', hp: 100 },
+      { id: 2, name: 'ゴブリン', hp: 200 },
+      { id: 3, name: 'ドラゴン', hp: 500 }
+      ]
+    }
+  },
+}
+</script>
